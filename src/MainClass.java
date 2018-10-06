91b4b4cbb6559f519f4528e000607ac05a41e4f9
fomi4ok <32990229+fomi4ok@users.noreply.github.com>
2018-10-06 16:45:18 -0500

    first_task

---
+++ src/MainClass.java
@@ -0,0 +1,24 @@
+
+public class MainClass {
+
+  int number = 14;
+  private int class_number =45;
+  private String class_string = "Hello, world!";
+
+
+  public int getLocalNumber(){
+    return this.number;
+
+  }
+
+  public int getClassNumber() {
+    return this.class_number;
+
+  }
+
+  public String getClassString() {
+    return this.class_string;
+  }
+
+
+}
---
+++ src/MainClassTest.java
@@ -0,0 +1,36 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class MainClassTest {
+
+  MainClass mc = new MainClass();
+
+
+  @Test
+
+  public void testGetLocalNumber(){
+
+    Assert.assertEquals("GetLocalNumber doesn't return 14", 14, mc.getLocalNumber());
+  }
+
+  @Test
+
+  public void testGetClassNumber() {
+
+    Assert.assertTrue("the number returns less than 45", (mc.getClassNumber() > 45) == true);
+  }
+
+
+   @Test
+  public void testGetClassString() {
+
+     Assert.assertTrue("String doesn't contains word hello", (mc.getClassString().contains("Hello") || mc.getClassString().contains("hello")  )== true);
+  }
+
+
+
+}
+
+
+
